e749e6fb110bdefa0469ecbf81e431b30c371f50
susli <suslikov0080@gmail.com>
2020-05-12 21:28:22 +0200

    movie_3

--- Projeckts/NewProjectMove/script.js
+++ Projeckts/NewProjectMove/script.js
@@ -31,21 +31,28 @@
 
 4) Потренироваться и переписать цикл еще двумя способами*/
 
-
-
 // Код возьмите из предыдущего домашнего задания
+/*                  Задание на урок:
+1) Первую часть задания повторить по уроку
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+P.S. Функции вызывать не обязательно */
 "use strict";
 
+let numberOfFilms;
 
-
-
-
-
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "")
+function start(){
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
 
     while(isNaN(numberOfFilms) || numberOfFilms == "" ||  numberOfFilms == null){
         numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");    
-};
+    }
+}
+start();
+
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -55,35 +62,49 @@ const personalMovieDB = {
     privat: false
 };
    
-    // for(let i = 0; i < 2; i++){
-    //        const a = prompt('Один из последних просмотренных фильмов?', ''),
-    //              b = prompt('На сколько оцените его?', '');
-    //             personalMovieDB.movies[a] = b ;
-   // }; 
-   let i = 0;  
-    // while(i<2){
-    //     const a = prompt('Один из последних просмотренных фильмов?', ''),
-    //              b = prompt('На сколько оцените его?', '');
-    //             personalMovieDB.movies[a] = b ;
-    //             i++;
-    //}; 
-    do{
-       const a = prompt('Один из последних просмотренных фильмов?', ''),
-                 b = prompt('На сколько оцените его?', '');
-                personalMovieDB.movies[a] = b ; 
-                i++;
-    }while(i < 2)
+function rememberMyFilms (){   
+    for (let i = 0; i < 2; i++){
+            let a = prompt('Один из последних просмотренных фильмов?', ''),
+                b = prompt('На сколько оцените его?', '');
+               
+        if (a != null && b != null && a != "" && b !="" && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log("Всё верно");       
+        } else {
+        console.log('Ошибка');
+            i=i-1;
+        }
+   }
+}
 
-        
-        
-if(personalMovieDB.count < 10){
-    console.log("Просмотрено довольно мало фильмов");
-}else if (personalMovieDB.count > 10 && personalMovieDB.count < 30 ){
-    console.log("Вы классический зритель");
-}else if (personalMovieDB.count > 30 ){
-    console.log("Вы киноман");
-}else{       
- console.log("Произошла ошибка");
-};
+rememberMyFilms (); 
 
+function detectPersonalLevel(){      
+    if(personalMovieDB.count < 10){
+        console.log("Просмотрено довольно мало фильмов");
+    }else if (personalMovieDB.count > 10 && personalMovieDB.count < 30 ){
+        console.log("Вы классический зритель");
+    }else if (personalMovieDB.count >= 30 ){
+        console.log("Вы киноман");
+    }else{       
+        console.log("Произошла ошибка");
+    }
+}
 
+detectPersonalLevel();
+
+function showMyDB (hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+showMyDB(personalMovieDB.privat);
+
+function writeYouGenres () {
+    for (let i = 1; i <= 3; i++) {
+        const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+        personalMovieDB.genres[i - 1] = genre;
+    }
+}
+
+writeYouGenres ();
--- Projeckts/Newproject/skript.js
+++ Projeckts/Newproject/skript.js
@@ -1,11 +1,15 @@
 "use strict";
 
-let user = {
-	name: "Вася"
-};
+let  logg = "HELLO WORLD",
+     str = "ratatuy";
+console.log(logg.slice(0, 5));
+console.log(str.slice(0, 7));
+console.log(logg.slice(6, 11));
+console.log(logg.indexOf("WORLD"));
+console.log(logg.slice(4));
 
-let id = Symbol("id");
+let num = "3.5";
 
-user[id = 1;
-
-alert( user[id] );
+console.log(Math.round(num));
+console.log(parseFloat(num));
+console.log(parseInt(num));	
